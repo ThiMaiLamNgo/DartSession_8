39d4c567bc6c28c8a8ab8ecc34b0cbfbcf008f5a
ThiMaiLamNgo <91917023+ThiMaiLamNgo@users.noreply.github.com>
2021-10-15 20:24:55 +0000

    Update ch7.dart

--- bin/ch7.dart
+++ bin/ch7.dart
@@ -14,12 +14,18 @@
 
  */
 
-void getYesNo(){
-  // Code from ch6 solution should be used inside here
-
+import 'dart:io';
+bool getYesNo(String prompt){
+  print(prompt);
+  String userAns = stdin.readLineSync();
+  bool ans;
+  if (userAns.toLowerCase() == 'yes'){
+    ans = true;
+  } else if (userAns.toLowerCase() == 'no'){
+    ans = false;
+  }
+  return ans;
 }
-
-
 void main() {
   // You should see an error here before you edit getYesNo, can you see why?
   bool output = getYesNo('Are whales mammals?');
