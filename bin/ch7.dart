155f7181be07d0f90203195554cf51de2dc02341
ThiMaiLamNgo <91917023+ThiMaiLamNgo@users.noreply.github.com>
2021-10-15 21:55:45 +0000

    Update ch7.dart

--- bin/ch7.dart
+++ bin/ch7.dart
@@ -32,5 +32,46 @@ void main() {
   print('Are whales mammals? : Answer $output');
 }
 /*
+Calling challenge5.dart to this challenge and using while loop to ask user until he enters yes or no:
+import 'dart:io';
+import 'challenge5.dart';
+bool  getYesNo(String prompt){
+  print(prompt);
+  bool ans;
+  while (ans == null ){
+    String userAns = stdin.readLineSync();
+    print(prompt);
+    if (userAns.toLowerCase() == 'yes'){
+      ans = true;
+    } else if (userAns.toLowerCase() == 'no'){
+      ans = false;
+    }
+  }
+  return ans;
+}
+void main(){
+  bool output = getYesNo('Are whales mammals?');
+  print('Answer $output');
+}
+/* challenge5.dart 
+import 'dart:io';
+double getDouble(String prompt){
+  return double.parse(getString(prompt));
+}
+int getInt(String prompt){
+  print(prompt);
+  return int.parse(stdin.readLineSync());
+}
+String getString(String prompt){
+  print(prompt);
+  return stdin.readLineSync();
+}
+
+void main(){
+  bool output = getYesNo('Are whales mammals?');
+  print('Answer $output');
+}
+
+*/
 
 */
